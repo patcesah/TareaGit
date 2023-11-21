3741f6d8b9d6aeb12faef27d5212cd7ec1ec7966
patcesah <87942932+patcesah@users.noreply.github.com>
2023-11-20 23:15:26 -0600

    multiplicar

--- lista.py
+++ lista.py
@@ -4,6 +4,14 @@ def suma_lista(input_lista):
         suma_total += elemento
     return suma_total
 
+def multiplicar_lista(input_lista):
+    multiplicar_total = 0 
+    for valor in input_lista:
+        multiplicar_total *= valor
+    return multiplicar_total
+
 lista = [10, 9, 8, 7, 6, 5]
-resultado = suma_lista(lista)
-print(resultado)
+resultado_suma = suma_lista(lista)
+resultado_multiplicar = multiplicar_lista(lista)
+print(resultado_suma)
+print(resultado_multiplicar)
